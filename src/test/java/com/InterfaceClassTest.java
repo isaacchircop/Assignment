2ebb6021d1dabac0a93ca4db7d646b7db05f0e36
Isaac <isaacchircop@gmail.com>
2014-04-30 17:15:48 +0200

    Changed tests

--- src/test/java/com/InterfaceClassTest.java
+++ src/test/java/com/InterfaceClassTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 import com.Interface;
 import com.Player;
 import org.junit.After;
@@ -11,72 +7,40 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Daniel
- */
 public class InterfaceClassTest {
     
     public InterfaceClassTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-        
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-        
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
+
     @Test
-    public void testPlayerNumberCheckValidPlayers() {        
-        assertTrue("Checking whether 7 players are valid", Interface.playerNumberCheck(7));
+    public void testingValidPlayers() {
+
+        assertTrue("Checking validity 7 players", Interface.playerNumberCheck(7));
+
     }
+
     @Test
-    public void testPlayerNumberCheckInvalidPlayers() {
+    public void testingInvalidPlayers() {
         
-        assertFalse("Checking whether 10 players are valid", Interface.playerNumberCheck(10));
+        assertFalse("Checking validity of 10 players", Interface.playerNumberCheck(10));
+        assertFalse("Checking validity of 1 player", Interface.playerNumberCheck(1));
+
     }
-    @Test 
-    public void testMapSizeCheckValidSize1() {
+
+    @Test
+    public void testValidMapSize() {
         
-        assertTrue("Checking whether a map size of 7 is valid with 2 players", Interface.mapSizeCheck(5, 2));
+        assertTrue("Checking validity of map size of 5 for 2 players", Interface.mapSizeCheck(5, 2));
+        assertTrue("Checking validity of map size of 10 for 4 players", Interface.mapSizeCheck(10, 4));
+
     }
-    @Test 
-    public void testMapSizeCheckValidSize2() {
+
+    @Test
+    public void testInvalidMapSize() {
         
-        assertTrue("Checking whether a map size of 10 is valid with 6 players", Interface.mapSizeCheck(10, 6));
-    }
-    @Test 
-    public void testMapSizeCheckInvalidSize1() {
-        
+        assertFalse("Checking whether a map size of 5 is valid with 6 players", Interface.mapSizeCheck(5, 6));
         assertFalse("Checking whether a map size of 2 is invalid with 2 players", Interface.mapSizeCheck(2, 2));
+
     }
-    @Test 
-    public void testMapSizeCheckInvalidSize2() {
-        
-        assertFalse("Checking whether a map size of 66 is invalid with 2 players", Interface.mapSizeCheck(66, 2));
-    }
-    @Test 
-    public void testMapSizeCheckInvalidSize3() {
-        
-        assertFalse("Checking whether a map size of 100 is invalid with 6 players", Interface.mapSizeCheck(100, 6));
-    }
-    @Test 
-    public void testMapSizeCheckInvalidSize4() {
-        
-        assertFalse("Checking whether a map size of 5 is invalid with 6 players", Interface.mapSizeCheck(5, 6));
-    }
+    
 }
