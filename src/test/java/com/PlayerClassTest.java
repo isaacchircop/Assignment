189bad077f034c91ec02b0c6a79cb751d1746932
Isaac <isaacchircop@gmail.com>
2014-05-01 21:21:36 +0200

    trying to change files on github

--- src/test/java/com/PlayerClassTest.java
+++ src/test/java/com/PlayerClassTest.java
@@ -1,18 +1,9 @@
-package com;/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package com;
 
 import org.junit.*;
-
 import java.io.File;
-
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Daniel
- */
 public class PlayerClassTest {
 
     public PlayerClassTest() {
@@ -20,34 +11,46 @@ public class PlayerClassTest {
     
     @Test
     public void testGetHTML() {
+
         Player p1 = new Player(0,0,0);
         assertTrue("Testing whether getHTML() returns a File type object",p1.getHTML() instanceof File);
+
     }
     @Test
     public void testGetCSS() {
+
         Player p1 = new Player(0,0,0);
         assertTrue("Testing whether getCSS() returns a File type object",p1.getCSS() instanceof File);
+
     }
     @Test
     public void testGetCurrentRow() {
+
         Player p1 = new Player(0,0,0);
         p1.updatePosition(3,4);
         assertEquals("Testing whether getCurrentRow() returns 3",p1.getCurrentRow(),3);
+
     }
     @Test
     public void testGetCurrentColumn() {
+
         Player p1 = new Player(0,0,0);
         p1.updatePosition(3,4);
         assertEquals("Testing whether getCurrentCol() returns 4",p1.getCurrentCol(),4);
+
     }
     @Test
     public void testGetInitRow() {
+
         Player p1 = new Player(5,6,0);
         assertEquals("Testing whether getInitRow() returns 5",p1.getInitRow(),5);
+
     }
     @Test
     public void testGetInitColumn() {
+
         Player p1 = new Player(5,6,0);
         assertEquals("Testing whether getInitColumn() returns 6",p1.getInitCol(),6);
+
     }
 }
