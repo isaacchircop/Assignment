8cee5bff488e8e7825dd9cd2ade5dfeca59092b3
Isaac <isaacchircop@gmail.com>
2014-05-01 21:52:12 +0200

    updated src folder

--- src/main/java/com/Interface.java
+++ src/main/java/com/Interface.java
@@ -4,14 +4,15 @@ import java.util.*;
 
 public class Interface {
 
-    public static void main(String args[]) {
+    public static void main (String args[]) {
 
         // Get number of players and map size
+
         int numPlayers = getPlayers();
         int size = getMapSize(numPlayers);
 
         // Create Map and Players
-        Map map = new Map(size);
+        Map map = new Map (size);
         Player[] players = createPlayers(map, numPlayers);
 
         // Output Map to HTML Files
@@ -20,6 +21,162 @@ public class Interface {
         // Create inital CSS Files
         map.outputUpdatedMap(players);
 
+        boolean c;
+
+        if(checkNumberOfPlayers(players,players.length)){
+            if(checkPlayerTiles(players,map)){
+                do {
+                    c = performUpdate(players,map);
+                }while(!c);
+                System.out.println ("Game Finished!");
+
+            }
+            else {
+                System.out.println("Error Storing Players\n");
+            }
+
+
+        }
+    }
+
+
+    public static int getPlayers() {
+
+        System.out.print ("Number of players: ");
+
+        Scanner k = new Scanner (System.in);
+        int players = k.nextInt();
+
+
+        while (!playerNumberCheck(players)) {
+
+
+            System.out.println ("\nInvalid Input!  Minimum: 2 Players, Maximum: 8 Players\n");
+
+            System.out.print ("Number of players: ");
+            players = k.nextInt();
+        }
+
+        return players;
+
+    }
+
+    public static int getMapSize(int players) {
+
+        Scanner k = new Scanner (System.in);
+
+        System.out.print ("\nMap height and width (n): ");
+        int n = k.nextInt();
+
+        if (players <= 4) {
+
+            while (!mapSizeCheck(n,players)) {
+
+                System.out.println ("\nInvalid Input!  Minimum n: 5, Maximum n: 50\n");
+
+                System.out.println ("Map height and width (n): ");
+                n = k.nextInt();
+
+            }
+
+        } else {
+
+            while (!mapSizeCheck(n,players)) {
+
+                System.out.println ("\nInvalid Input!  Minimum n: 8, Maximum n: 50\n");
+
+                System.out.println ("Map height and width (n): ");
+                n = k.nextInt();
+
+            }
+
+        }
+
+        return n;
+
+    }
+
+    public static Player[] createPlayers (Map map, int players) {
+
+        int length = map.getLength();
+
+        Player[] playersArray = new Player[players];
+
+        Random rand = new Random();
+        int row, col;
+
+        for (int i = 0; i < players; i++) {
+
+            do {
+
+                row = rand.nextInt(length);
+                col = rand.nextInt(length);
+
+            } while (map.getTile(row, col) != Tile.Grass);
+
+            playersArray[i] = new Player(row,col, i);
+
+        }
+
+        return playersArray;
+
+    }
+
+    public static boolean playerNumberCheck(int players) {
+
+        if (players < 2 || players > 8) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public static boolean mapSizeCheck(int size, int players){
+
+        if((players <= 4) && (size < 5 || size > 50)){
+            return false;
+        }
+        else if((players >= 5) && (size < 8 || size > 50)){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public static boolean checkPlayerTiles(Player[]  players, Map map){
+
+        int playernum = 0;
+        boolean greentileflag = true;
+        while(playernum < players.length){
+
+            int column = players[playernum].getCurrentCol();
+            int row = players[playernum].getCurrentRow();
+            if(map.getTile(row, column) != Tile.Grass){
+                greentileflag = false;
+                break;
+            }
+            playernum++;
+
+        }
+        return greentileflag;
+
+    }
+
+
+    public static boolean checkNumberOfPlayers(Player [] players, int playernum){
+        if(playernum == players.length){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public static boolean performUpdate(Player[] players, Map map){
+
         int rndCnt = 0;
 
         do {
@@ -27,9 +184,9 @@ public class Interface {
             rndCnt++;
 
             System.out.println("\n===================================================");
-            System.out.println("Round " + rndCnt + ": Move (U)p (D)own (L)eft or (R)ight\n");
+            System.out.println ("Round " + rndCnt + ": Move (U)p (D)own (L)eft or (R)ight\n");
 
-            Scanner k = new Scanner(System.in);
+            Scanner k = new Scanner (System.in);
 
             for (int i = 0; i < players.length; i++) {
 
@@ -37,7 +194,7 @@ public class Interface {
 
                 do {
 
-                    System.out.print("Awaiting Input from Player " + i + ": ");
+                    System.out.print ("Awaiting Input from Player " + i + ": ");
 
                     char move = k.next().toUpperCase().charAt(0);
 
@@ -72,139 +229,34 @@ public class Interface {
 
                         default:
 
-                            System.out.println("Invalid Move!  Please input one of these character options: U, D, L, R");
+                            System.out.println ("Invalid Move!  Please input one of these character options: U, D, L, R");
                             validCharacter = false;
                             break;
 
                     }
 
-                    if (validCharacter) {
+                    if (validCharacter == true) {
 
                         validMove = map.checkInRange(row, col);
 
                         if (validMove) {
 
-                            players[i].updatePosition(row, col);
+                            players[i].updatePosition (row, col);
 
                         } else {
 
-                            System.out.println("Invalid Move!  You tried to move outside map limit.  Please try again");
+                            System.out.println ("Invalid Move!  You tried to move outside map limit.  Please try again");
 
                         }
 
                     }
 
-                } while (!validMove);
+                } while (validMove == false);
 
             }
 
-        } while (!map.outputUpdatedMap(players));
-
-        System.out.println("Game Finished!");
-
-    }
-
-    public static int getPlayers() {
-
-        System.out.print("Number of players: ");
-
-        Scanner k = new Scanner(System.in);
-        int players = k.nextInt();
-
-        while (!playerNumberCheck(players)) {
-
-            System.out.println("\nInvalid Input!  Minimum: 2 Players, Maximum: 8 Players\n");
-
-            System.out.print("Number of players: ");
-            players = k.nextInt();
-
-        }
-
-        return players;
+        } while (map.outputUpdatedMap(players) == false);
+        return true;
 
     }
-
-    public static int getMapSize(int players) {
-
-        Scanner k = new Scanner(System.in);
-
-        System.out.print("\nMap height and width (n): ");
-        int n = k.nextInt();
-
-        while (!mapSizeCheck(n, players)) {
-
-            if (players <= 4) {
-
-                System.out.println("\nInvalid Input!  Minimum n: 5, Maximum n: 50\n");
-
-            } else {
-
-                System.out.println("\nInvalid Input!  Minimum n: 8, Maximum n: 50\n");
-
-            }
-
-            System.out.print("Map height and width (n): ");
-            n = k.nextInt();
-
-        }
-
-        return n;
-
-    }
-
-    public static Player[] createPlayers(Map map, int players) {
-
-        int length = map.getLength();
-
-        Player[] playersArray = new Player[players];
-
-        Random rand = new Random();
-        int row, col;
-
-        for (int i = 0; i < players; i++) {
-
-            do {
-
-                row = rand.nextInt(length);
-                col = rand.nextInt(length);
-
-            } while (map.getTile(row, col) != Tile.Grass);
-
-            playersArray[i] = new Player(row, col, i);
-
-        }
-
-        return playersArray;
-
-    }
-
-    public static boolean playerNumberCheck(int players) {
-
-        if (players < 2 || players > 8) {
-
-            return false;
-
-        } else {
-
-            return true;
-
-        }
-    }
-
-    public static boolean mapSizeCheck(int size, int players) {
-
-        if ((players <= 4) && (size < 5 || size > 50)) {
-
-            return false;
-
-        } else if ((players >= 5) && (size < 8 || size > 50)) {
-
-            return false;
-
-        } else {
-
-            return true;
-        }
-    }
-
 }
--- src/test/java/com/MapClassTest.java
+++ src/test/java/com/MapClassTest.java
@@ -1,7 +1,19 @@
-package com;
+
+package com;/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 
 import com.Map;
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class MapClassTest {
